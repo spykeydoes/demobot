e785ec009f14cd147a5e2118f92153c4fc647269
James Cuttell <jcut8453@SHARED.SYDNEY.EDU.AU>
2019-01-10 17:04:50 +1100

    Now with names

--- demobot.py
+++ demobot.py
@@ -3,9 +3,8 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/greet', methods=['GET', 'POST'])
-def hello_world():
+def greet_person():
     name = request.values.get('text')
     return f'Hi {name}!'
 
-if __name__ == '__main__':
-    app.run()
+app.run(debug=True)
