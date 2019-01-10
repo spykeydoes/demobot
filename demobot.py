7663dcccdf815e854030340bbe22e09e10a47685
James Cuttell <jcut8453@SHARED.SYDNEY.EDU.AU>
2019-01-10 17:10:05 +1100

    Added if statement before app.run

--- demobot.py
+++ demobot.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def greet_person():
     name = request.values.get('text')
     return f'Hi {name}!'
-
-app.run(debug=True)
+    
+if __name__ == __main__:
+    app.run(debug=True)
