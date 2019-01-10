80f31ed9f6673a94c3d462af450c5d7c1bce49cc
James Cuttell <jcut8453@shared.sydney.edu.au>
2019-01-10 15:52:44 +1100

    Removed Comments

--- demobot.py
+++ demobot.py
@@ -1,15 +1,10 @@
-# Import flask
 from flask import Flask
 
-# Create your app (web server)
 app = Flask(__name__)
 
-
-# When people visit the home page '/' use the hello_world function
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Yeet'
 
 if __name__ == '__main__':
-    # Start the web server!
     app.run()
