c92a0512621a0cb063ead186ee447295b0d0a0f7
James Cuttell <jcut8453@shared.sydney.edu.au>
2019-01-10 17:01:29 +1100

    Now ask names

--- demobot.py
+++ demobot.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/greet', methods=['GET', 'POST'])
 def hello_world():
-    return 'Yeet'
+    name = request.values.get('text')
+    return f'Hi {name}!'
 
 if __name__ == '__main__':
     app.run()
