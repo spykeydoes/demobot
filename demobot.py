f68a812495d1e02c9f51e7c4ae8f797eaec2fac4
James Cuttell <jcut8453@SHARED.SYDNEY.EDU.AU>
2019-01-10 17:24:58 +1100

    Making it unique

--- demobot.py
+++ demobot.py
@@ -2,10 +2,10 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/greet', methods=['GET', 'POST'])
+@app.route('/yeet', methods=['GET', 'POST'])
 def greet_person():
     name = request.values.get('text')
-    return f'Hi {name}!'
+    return f'Hi Memelord {name}!'
 
 if __name__ == "__main__":
     app.run(debug=True)
